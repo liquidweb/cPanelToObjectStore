bc3dbc5af82785531f404da8dccfae4597dda8b5
Jason Gillman Jr <jgillman@liquidweb.com>
2015-04-01 07:46:08 -0400

    Use threads for concurrent uploads of multipart sends

--- cPanelToLWObjStore.py
+++ cPanelToLWObjStore.py
@@ -9,6 +9,8 @@ from boto.s3.connection import S3Connection
 from boto.s3.bucket import Bucket
 from boto.s3.key import Key
 from filechunkio import FileChunkIO
+from Queue import Queue
+from threading import Thread
 
 ### Get the command line args ###
 command, pwd = sys.argv[1:3]
@@ -43,6 +45,29 @@ logging.basicConfig(filename = logPath, level=logging.INFO)
 def logTime():
 	return time.strftime('%Y-%m-%d %H:%M:%S')
 
+### Setup threading
+threadList = [] # So we can iterate against this and setup the join.
+THREADS = 10; # Adjust as needed
+
+class chunkWorker(Thread):
+	def __init__(self, queue):
+		self.queue = queue
+		Thread.__init__(self)
+	def run(self):
+		while not self.queue.empty():
+			jobData = self.queue.get()
+
+			multiPart = jobData['multiPart']
+			fp = jobData['fp']
+			part_num = jobData['part_num']
+			chunkCount = jobData['chunkCount']
+
+			logMsg = logTime() + ' :: Sending chunk ' + str(part_num) + ' of ' + str(chunkCount)
+			logging.info(logMsg)
+			multiPart.upload_part_from_file(fp, part_num)
+
+		return
+
 ### Functions by command ###
 
 def get(remoteFile, localFile):
@@ -57,6 +82,7 @@ def put(localFile, remoteFile):
 		logging.info(logTime() + ' :: Sending the file in one chunk')
 		key.key = remoteFile
 		key.set_contents_from_filename(localFile)
+
 	else:
 		logging.info(logTime() + ' :: Sending file in multiple parts')
 		multiPart = objStoreBucket.initiate_multipart_upload(remoteFile)
@@ -65,16 +91,33 @@ def put(localFile, remoteFile):
 		chunkSize = 100000000 # 100M Decimal
 		chunkCount = int(math.ceil(fileSize / float(chunkSize)))
 
-		# Send the parts
+		# Build queue and queue the parts
+		partQueue = Queue(maxsize = 0)
+
 		for i in range(chunkCount):
-			logMsg = logTime() + ' :: Sending chunk ' + str(i + 1) + ' of ' + str(chunkCount)
-			logging.info(logMsg)
+			
 
 			offset = chunkSize * i
 			byteCount = min(chunkSize, fileSize - offset)
 
-			with FileChunkIO(localFile, 'r', offset = offset, bytes = byteCount) as fp:
-				multiPart.upload_part_from_file(fp, part_num = i + 1)
+			fp = FileChunkIO(localFile, 'r', offset = offset, bytes = byteCount)
+			queueData = {
+				'fp': fp,
+				'part_num': i + 1,
+				'multiPart': multiPart,
+				'chunkCount': chunkCount
+			}
+			
+			partQueue.put(queueData)
+
+		# Run the queue
+		for i in range(THREADS):
+			t = chunkWorker(partQueue)
+			threadList.append(t)
+			t.start()
+
+		for t in threadList:
+			t.join()
 
 		# Finish the send
 		logging.info(logTime() + ' :: Combining file parts')
